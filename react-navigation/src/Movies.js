cbe5a9dbc35371cde71eb9381a93dacc24e1a912
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-10-28 11:40:25 +0530

    first commit

--- react-navigation/src/Movies.js
+++ react-navigation/src/Movies.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import AddMovie from './AddMovie';
 import MovieList from './MovieList';
 
@@ -6,40 +6,59 @@ import MovieList from './MovieList';
 function Movies() {
 
     const [movies, setMovies] = useState([]);
-    function fetchMovieHandler() {
-        fetch('https://collmcool-default-rtdb.firebaseio.com/movies.json').then((response) => {
-            return response.json();
-        }).then((data) => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    const fetchMovieHandler = useCallback(async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch('https://collmcool-default-rtdb.firebaseio.com/movies.json');
+            if (!response.ok) {
+                throw new Error('Something went Wrong');
+            }
+            const data = await response.json();
             const transformedMovies = [];
-            console.log(data);
-            for(const key in data)
-            {
+            for (const key in data) {
                 transformedMovies.push(
                     {
-                        id:key,
-                        title:data[key].title,
-                        openingText:data[key].openingText,
-                        releaseDate:data[key].releaseDate,
+                        id: key,
+                        title: data[key].title,
+                        openingText: data[key].openingText,
+                        releaseDate: data[key].releaseDate,
                     }
                 );
             }
             setMovies(transformedMovies);
-            // });
-        });
-    }
+        } catch (error) {
+            setError(error.message);
+        }
+        setIsLoading(false);
+    }, []);
 
-    async function  addMovieHandler(movie) {
-        
+    let content=<p>Found no Movies</p>;
+    if(movies.length>0){
+        content=<MovieList movies={movies} />;
+    }
+    if(isLoading){
+        content=<p>Loading </p>
+    }
+    if(error){
+        content=<p>{error} </p>
+    }
+    async function addMovieHandler(movie) {
+
         console.log(movie);
-       const reponse= await fetch('https://collmcool-default-rtdb.firebaseio.com/movies.json',{
-           method:'Post',
-           body:JSON.stringify(movie),
-           headers:{
-               'Content-Type':'application/json'
-           }
-       });
-       const data=await reponse.json();
-       console.log(data);
+        const reponse = await fetch('https://collmcool-default-rtdb.firebaseio.com/movies.json', {
+            method: 'Post',
+            body: JSON.stringify(movie),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await reponse.json();
+        console.log(data);
     }
 
     return (
@@ -51,7 +70,7 @@ function Movies() {
                 <button onClick={fetchMovieHandler}> Fetch Movie</button>
             </section>
             <section>
-                <MovieList movies={movies} />
+                {content}
             </section>
         </>
     );
